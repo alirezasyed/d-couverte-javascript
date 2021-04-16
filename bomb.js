d60bed1965ed7ff1b0b7474062472635e33cbbd1
alirezasyed <codingchallenge2020@yahoo.com>
2021-04-16 10:12:05 -0700

    ajoute le fichier bombe et mis en place la fonction de creation de bombe

---
+++ bomb.js
@@ -0,0 +1,61 @@
+let bombes = [];
+
+for (var i = 0; i < 3; i++) {
+    bombe = document.createElement('div');
+    bombe.style.width = GRID_SIZE + "px";
+    bombe.style.height = GRID_SIZE + "px";
+    bombe.style.backgroundColor = "red";
+    bombe.style.backgroundSize = "contain";
+    bombe.style.position = 'absolute';
+    bombe.style.zIndex = "100";
+    bombe.libre = true;
+    bombe.id = "bombe" + String(i);
+    bombes.push(bombe);
+}
+
+function createBombe(blockGrid) {
+    let bombeset = false
+    for (var i = 0; i < bombes.length; i++) {
+        if (bombes[i].libre && !bombeset) {
+
+            var bombe = bombes[i];
+            bombe.libre = false;
+            bombeset = true;
+
+            bombe.style.left = String(x * GRID_SIZE) + "px";
+            bombe.style.top = String(y * GRID_SIZE) + "px";
+            bombe.x = x;
+            bombe.y = y;
+
+            blockGrid[bombe.x][bombe.y].traverser = false;
+            blockGrid[bombe.x][bombe.y].bombe = true;
+            document.getElementById('plateau').appendChild(bombe);
+
+            setTimeout(function explose() {
+                if (bombe.x < H_GRID - 1) {
+
+                    blockGrid[bombe.x + 1][bombe.y].traverser = true;
+                    blockGrid[bombe.x + 1][bombe.y].style.backgroundColor = "green";
+                }
+                if (bombe.x > 0) {
+
+                    blockGrid[bombe.x - 1][bombe.y].traverser = true;
+                    blockGrid[bombe.x - 1][bombe.y].style.backgroundColor = "green";
+                }
+                if (bombe.y < V_GRID - 1) {
+
+                    blockGrid[bombe.x][bombe.y + 1].traverser = true;
+                    blockGrid[bombe.x][bombe.y + 1].style.backgroundColor = "green";
+                }
+                if (bombe.y > 0) {
+
+                    blockGrid[bombe.x][bombe.y - 1].traverser = true;
+                    blockGrid[bombe.x][bombe.y - 1].style.backgroundColor = "green";
+                }
+
+
+            }, 2000);
+
+        }
+    }
+}
